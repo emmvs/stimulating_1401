b31ad1b7dbefa6d748b59fc5258726d524e426b3
Emma A. A. Rünzel <emma@ruenzel.de>
2023-11-08 13:26:38 +0100

    Update Index & Application.js For Stimulus Setup & Movies Controller Integration

--- javascript/application.js
+++ javascript/application.js
@@ -1,13 +1,21 @@
-// Stimulus Setup II
+// Bring the necessary Stimulus classes into your files
 import { Application, Controller } from 'stimulus';
+
+// Initialize the Stimulus application and makes it globally accessible
 window.Stimulus = Application.start()
 
+// Import Controllers
+import MoviesController from './controllers/movies_controller.js'
 import DisableButtonController from "./controllers/disable_button_controller.js"
+
+// Register controllers w/ their identifiers so Stimulus knows
+// what code to execute when a matching data-controller is found in the HTML
 Stimulus.register("disable-button", DisableButtonController)
+Stimulus.register('movies', MoviesController)
 
 // const button = document.querySelector("#click-me")
 // button.addEventListener("click", (event) => {
-//   // console.log(event);
+//   console.log(event);
 //   event.currentTarget.classList.remove("btn-primary");
 //   event.currentTarget.classList.add("btn-danger");
 //   event.currentTarget.innerText = "Bingo!";
--- javascript/controllers/disable_button_controller.js
+++ javascript/controllers/disable_button_controller.js
@@ -1,8 +1,20 @@
+// Import Controller class from Stimulus framework
+// Controllers are the CORE CONCEPTS of Stimulus
+// - to add behavior to the HTML
+// - by connecting them to DOM elements
 import { Controller } from 'stimulus'
 
+// Creates new Controller class without name (anonymous class)
+// Exports it as the default export of the module
+// Meaning: It not only inherits all the functionalities of a Stimulus Controller
+// but also get all the additional functionalities we give to it
 export default class extends Controller {
   static targets = ["button", "link"];
 
+  // Connect Method
+  // Special lifecycle callback provided by Stimulus | similar to hooks
+  // - lifecycle call back means it is automatically called by Stimulus
+  // - when the controller is connected to the DOM through rendering HTML element (data-controller)
   connect() {
     console.log("👻");
   }
